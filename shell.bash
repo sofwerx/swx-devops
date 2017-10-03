9b6a5a318518f28f8a40fe0ca91acdccabbebbe7
Ian Blenke <ian@blenke.com>
2017-10-03 15:13:18 -0400

    Alow AWS_PROFILE to drive the access key retreival

--- shell.bash
+++ shell.bash
@@ -3,11 +3,11 @@ devops=$(cd -P -- "$(dirname -- "$0")" && pwd -P)
 
 export AWS_CONFIG_FILE=${AWS_CONFIG_FILE:-~/.aws/config}
 export AWS_PROFILE=${AWS_PROFILE:-sofwerx}
-export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID:-$(aws configure get aws_access_key_id --profile $AWS_PROFILE)}
-export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY:-$(aws configure get aws_secret_access_key --profile $AWS_PROFILE)}
+export AWS_ACCESS_KEY_ID=$(aws configure get aws_access_key_id --profile $AWS_PROFILE)
+export AWS_SECRET_ACCESS_KEY=$(aws configure get aws_secret_access_key --profile $AWS_PROFILE)
 export AWS_REGION=${AWS_REGION:-$(aws configure get region --profile $AWS_PROFILE)}
 export AWS_DEFAULT_REGION=${AWS_REGION}
-export AWS_DEFAULT_OUTPUT=${AWS_DEFAULT_OUTPUT:-$(aws configure get output --profile $AWS_PROFILE)}
+export AWS_DEFAULT_OUTPUT=$(aws configure get output --profile $AWS_PROFILE)
 
 # Set the bash prompt to show our $AWS_PROFILE
 export PS1='[$AWS_PROFILE] \h:\W \u\$ '
