41891cc23b3908f9c5e2d94371d91ffdc3d1b47d
Ian Blenke <ian@blenke.com>
2017-10-06 16:26:12 -0400

    Fixing duplicate security group on 8080

--- aws/rcloud-dev/terraform/vpc.tf
+++ aws/rcloud-dev/terraform/vpc.tf
@@ -155,17 +155,6 @@ resource "aws_security_group_rule" "sg_ingress_all_icmpv6" {
     security_group_id = "${aws_security_group.sg.id}"
 }
 
-resource "aws_security_group_rule" "sg_ingress_nifi" {
-    type = "ingress"
-    from_port = 8080
-    to_port = 8080
-    protocol = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-    ipv6_cidr_blocks = ["::/0"]
-
-    security_group_id = "${aws_security_group.sg.id}"
-}
-
 resource "aws_security_group_rule" "sg_ingress_all_internal" {
     type = "ingress"
     from_port = 0
