ba04f3bc6dbf7a59c0f6d498cf42048fee3b40c3
Ian Blenke <ian@blenke.com>
2017-10-09 14:32:22 -0400

    Updating rcloud-dev security group rules for http+https instead of just web on 80

--- aws/rcloud-dev/terraform/vpc.tf
+++ aws/rcloud-dev/terraform/vpc.tf
@@ -101,10 +101,21 @@ resource "aws_security_group_rule" "sg_ingress_ssh" {
     security_group_id = "${aws_security_group.sg.id}"
 }
 
-resource "aws_security_group_rule" "sg_ingress_web" {
+resource "aws_security_group_rule" "sg_ingress_http" {
     type = "ingress"
     from_port = 80
     to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+    security_group_id = "${aws_security_group.sg.id}"
+}
+
+resource "aws_security_group_rule" "sg_ingress_https" {
+    type = "ingress"
+    from_port = 443
+    to_port = 443
     protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
