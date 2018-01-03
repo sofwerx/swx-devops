e9cc0095d97cce6cdea46471bd8b7a138727a8bf
Ian Blenke <ian@blenke.com>
2018-01-03 12:51:51 -0500

    Updating the user-data.sh script for jumpbox-kali, yet again

--- aws/jumpbox-kali/user-data.sh
+++ aws/jumpbox-kali/user-data.sh
@@ -2,43 +2,35 @@
 
 export DEBIAN_FRONTEND=noninteractive
 
-mkdir -p /etc/dhcp/dhclient-exit-hooks.d/ 
-cat <<EOF > /etc/dhcp/dhclient-exit-hooks.d/sethostname
-if [ -n '$${hostname}' -a '$${hostname}' != "\$\$"'{hostname}' ]; then
-  hostname $${hostname}
+## Discover public and private IPv4 addresses for this instance
+PUBLIC_IPV4="$(curl -qs http://169.254.169.254/latest/meta-data/public-ipv4)"
+PRIVATE_IPV4="$(curl -qs http://169.254.169.254/latest/meta-data/local-ipv4)"
+
+mac=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ | head -1 | cut -d/ -f1)
+PUBLIC_IPV6=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$mac/ipv6s | head -1 | cut -d: -f1-4)
+
+echo "iface eth0 inet6 dhcp" > /etc/network/interfaces.d/60-default-with-ipv6.cfg
+sudo dhclient -6
+
+## Install some extra things
+sudo apt-get update
+export DEBIAN_FRONTEND=noninteractive
+sudo -E apt-get install -y fail2ban dnsmasq
+
+# Deal with AWS split-horizon DNS and using both IPV4 and IPV6 DNS servers
+
+# Disable resolvconf updates, because we can't have nice things.
+resolvconf --disable-updates
+
+mkdir -p /var/run/dnsmasq/
+if [ ! -f /var/run/dnsmasq/resolv.conf ] ; then
+  cp /etc/resolv.conf /var/run/dnsmasq/resolv.conf
 fi
-chmod +x /etc/dhcp/dhclient-exit-hooks.d/sethostname
-EOF
 
-if [ ! -f /.donotdeleteme ]; then
-  touch /.donotdeleteme
+domains="$(grep domain-name /var/lib/dhcp/dhclient.eth0.leases | awk '{print $3}' | cut -d';' -f1 | grep '"' | cut -d'"' -f2)"
+servers="$(grep domain-name-server /var/lib/dhcp/dhclient.eth0.leases | awk '{print $3}' | cut -d';' -f1)"
 
-  ## Discover public and private IPv4 addresses for this instance
-  PUBLIC_IPV4="$(curl -qs http://169.254.169.254/latest/meta-data/public-ipv4)"
-  PRIVATE_IPV4="$(curl -qs http://169.254.169.254/latest/meta-data/local-ipv4)"
-
-  mac=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/ | head -1 | cut -d/ -f1)
-  PUBLIC_IPV6=$(curl -s http://169.254.169.254/latest/meta-data/network/interfaces/macs/$mac/ipv6s | head -1 | cut -d: -f1-4)
-
-  echo "iface eth0 inet6 dhcp" > /etc/network/interfaces.d/60-default-with-ipv6.cfg
-  sudo dhclient -6
-
-  ## Install some extra things
-  sudo apt-get update
-  export DEBIAN_FRONTEND=noninteractive
-  sudo -E apt-get install -y fail2ban dnsmasq
-
-  # Deal with AWS split-horizon DNS and using both IPV4 and IPV6 DNS servers
-
-  # Disable resolvconf updates, because we can't have nice things.
-  resolvconf --disable-updates
-
-  ln -sf /var/run/resolvconf/interface/eth0.dhclient /var/run/dnsmasq/resolv.conf
-
-  domains="$(grep ^domain /var/run/dnsmasq/resolv.conf | cut -d' ' -f2-)"
-  servers="$(grep ^nameserver /var/run/dnsmasq/resolve.conf | cut -d' ' -f2-)"
-
-  cat <<EOC > /etc/dnsmasq.conf
+cat <<EOC > /etc/dnsmasq.conf
 interface=*
 port=53
 bind-interfaces
@@ -50,59 +42,56 @@ domain-needed
 all-servers
 EOC
 
-  ## Make sure we handle split-horizon for both ec2.internal and amazonaws.com
-  for domain in $domains amazonaws.com ; do
-    # Route ec2.internal to AWS servers by default
-    for server in $servers; do
-      echo 'server=/'"$domain"'/'"$server" >> /etc/dnsmasq.conf
-    done
+## Make sure we handle split-horizon for both ec2.internal and amazonaws.com
+for domain in $domains amazonaws.com ; do
+  # Route ec2.internal to AWS servers by default
+  for server in $servers; do
+    echo 'server=/'"$domain"'/'"$server" >> /etc/dnsmasq.conf
   done
+done
 
-  # Route all other queries, simultaneously, to both ipv4 and ipv6 DNS servers at Google
-  for server in 8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844 ; do
-    echo 'server=/*/'"$server" >> /etc/dnsmasq.conf
-  done
+# Route all other queries, simultaneously, to both ipv4 and ipv6 DNS servers at Google
+for server in 8.8.8.8 8.8.4.4 2001:4860:4860::8888 2001:4860:4860::8844 ; do
+  echo 'server=/*/'"$server" >> /etc/dnsmasq.conf
+done
 
-  cat <<EOR > /etc/resolv.conf
+cat <<EOR > /etc/resolv.conf
 search $domains
 nameserver $PRIVATE_IPV4
 EOR
 
-  /etc/init.d/dnsmasq restart
+/etc/init.d/dnsmasq restart
 
-  ## Install docker and enable it to use the IPV6 addresses
-  apt-get -y install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    software-properties-common
+## Install docker and enable it to use the IPV6 addresses
+apt-get -y install \
+  apt-transport-https \
+  ca-certificates \
+  curl \
+  software-properties-common
 
-  curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
 
-  add-apt-repository \
-   "deb [arch=amd64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
+add-apt-repository \
+ "deb [arch=amd64] https://download.docker.com/linux/debian \
+ $(lsb_release -cs) \
+ stable"
 
-  apt-get update
-  apt-get install -y linux-image-aws
-  apt-get install -y docker-ce
-  usermod -a -G docker admin
-  echo "User debian added to docker group. You may wish to re-login to avoid using sudo docker."  |wall
-
-fi
+apt-get update
+apt-get install -y linux-image-aws
+apt-get install -y docker-ce
+usermod -a -G docker admin
 
 USER_NAME=${USER_NAME:-manager}
 USER_HOME=${USER_HOME:-/home/${USER_NAME}}
 if ! id $USER_NAME ; then
   groupadd -g 1001 $USER_NAME
   useradd -g 1001 -u 1001 -d $USER_HOME -s /bin/bash -m $USER_NAME
+  usermod -aG sudo $USER_NAME
+  usermod -aG admin $USER_NAME
+  usermod -aG docker $USER_NAME
 fi
-usermod -aG sudo $USER_NAME
-usermod -aG admin $USER_NAME
-usermod -aG docker $USER_NAME
 
-# Add ssh key trust
+# Add ssh key trusts for manager user
 mkdir -p ${USER_HOME}/.ssh
 chown ${USER_NAME}.${USER_NAME} ${USER_HOME}/.ssh
 chmod 700 ${USER_HOME}/.ssh
