0004198636f5e3b5108b27e3e80a7ae47a993470
Ian Blenke <ian@blenke.com>
2018-01-11 22:07:50 -0500

    Moving out docker-marklogic out to its own github repo as a submodule

--- aws/marklogic-datahub/docker-marklogic/mlconfig.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#MASTER ML NODE CONFIGURATION PARAMETERS
-AUTH_MODE="anyauth"
-PASSWORD="password"
-USER="admin"
-SEC_REALM="public"
-LICENSE_FILE_FOLDER=/opt
-VERSION=8
-HOST_NAME=`hostname`
