c9a6bba10b17d6d176c9454457921ae17b66a331
Tracey Birch <tracey.birch@sofwerx.org>
2018-02-05 12:24:36 -0500

    Added a hint for swx commands

--- shell.bash
+++ shell.bash
@@ -1,3 +1,5 @@
 #!/bin/bash
 devops="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "*** Type swx followed by the tab key for swx-devops commands ***"
 exec bash --rcfile "${devops}/.bashrc" -i $@
+
