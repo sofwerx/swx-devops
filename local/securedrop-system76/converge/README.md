1e1c54b166247938592a429adc522601b0f72624
Ian Blenke <ian@blenke.com>
2018-02-20 16:39:21 -0500

    Adding securedrop-system76

---
+++ local/securedrop-system76/converge/README.md
@@ -0,0 +1,4 @@
+# docker-converge
+
+Using docker as a convergence tool to apply xrdp and other marklogic-datahub packages to a base ubuntu xenial build
+
---
+++ local/securedrop-system76/converge/converge.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+export DEBIAN_FRONTEND=noninteractive
+
+dpkg --configure -a --force-all
+
+### Update and install base packages ###
+apt-get update
+apt-get -y upgrade
+apt-get -y dist-upgrade
+apt-get -y autoremove --purge
+
+# Install latest xrdp
+cat <<EOF > /etc/apt/sources.list.d/xrdp.list
+deb http://ppa.launchpad.net/hermlnx/xrdp/ubuntu xenial main
+deb-src http://ppa.launchpad.net/hermlnx/xrdp/ubuntu xenial main
+EOF
+apt-key adv --keyserver keyserver.ubuntu.com --recv-keys CAB0B10F
+
+apt-get update
+apt-get install -y xrdp
+
+# Ensure `allowed_users=anybody` in /etc/X11/Xwrapper.config
+if ! grep -e ^allowed_users=anybody /etc/X11/Xwrapper.config ; then
+  if grep -e ^allowed_users /etc/X11/Xwrapper.config; then
+    sed -i -e 's/^needs_root_rights=.*$/allowed_users=anybody' /etc/X11/Xwrapper.config
+  else
+    echo "allowed_users=anybody" >> /etc/X11/Xwrapper.config 
+  fi
+fi
+
+# Ensure `needs_root_rights=no` in /etc/X11/Xwrapper.config
+if ! grep -e ^needs_root_rights=no /etc/X11/Xwrapper.config ; then
+  if grep -e ^needs_root_rights /etc/X11/Xwrapper.config; then
+    sed -i -e 's/^needs_root_rights=.*$/needs_root_rights=no/' /etc/X11/Xwrapper.config
+  else
+    echo needs_root_rights=no >> /etc/X11/Xwrapper.config
+  fi
+fi
+
+systemctl restart xrdp
+systemctl restart xrdp-sesman
+
+apt-get remove gnome-core
+apt-get install -y lxde-core lxde desktop-base
+update-alternatives –config x-session-manager /usr/bin/startlxde
+
+### Clean up ###
+apt-get -y autoremove --purge
+apt-get clean
+
