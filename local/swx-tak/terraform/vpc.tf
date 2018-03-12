d173a7fdc65c56495b3e8a25b844ece5f585b986
Ian Blenke <ian@blenke.com>
2018-03-12 11:00:59 -0400

    Set the correct ip for swx-tak

--- local/swx-tak/terraform/vpc.tf
+++ local/swx-tak/terraform/vpc.tf
@@ -21,7 +21,7 @@ resource "aws_route53_record" "project-name-cname" {
   name    = "${var.Lifecycle}.${var.dns_zone}"
   type    = "A"
   ttl     = "300"
-  records = ["172.109.143.90"]
+  records = ["172.109.143.94"]
 }
 
 /* Define a *.tak.devwerx.org CNAME record */
@@ -30,6 +30,6 @@ resource "aws_route53_record" "project-name-wildcard" {
   name    = "*.${var.Lifecycle}.${var.dns_zone}"
   type    = "A"
   ttl     = "300"
-  records = ["172.109.143.90"]
+  records = ["172.109.143.94"]
 }
 
