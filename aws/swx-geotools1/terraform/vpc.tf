2c4e3be7171e22a6693db0010dc679126875d3ec
Tracey Birch <tracey.birch@sofwerx.org>
2018-03-24 16:20:14 -0400

    add port for OSH

--- aws/swx-geotools1/terraform/vpc.tf
+++ aws/swx-geotools1/terraform/vpc.tf
@@ -188,6 +188,17 @@ resource "aws_security_group_rule" "sg_ingress_bolt" {
     security_group_id = "${aws_security_group.sg.id}"
 }
 
+resource "aws_security_group_rule" "sg_ingress_osh" {
+    type = "ingress"
+    from_port = 8181
+    to_port = 8181
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+
+    security_group_id = "${aws_security_group.sg.id}"
+}
+
 resource "aws_security_group_rule" "sg_ingress_structr" {
     type = "ingress"
     from_port = 8582
