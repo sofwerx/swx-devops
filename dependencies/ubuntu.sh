e14b744deb5eb013b2f83acf3d3ba6ec4aca9584
Tracey Birch <tracey.birch@sofwerx.org>
2018-05-04 14:17:31 -0400

    terraform update

--- dependencies/ubuntu.sh
+++ dependencies/ubuntu.sh
@@ -30,7 +30,7 @@ which vi > /dev/null || (
 )
 which terraform > /dev/null || (
   cd /tmp
-  wget https://releases.hashicorp.com/terraform/0.10.7/terraform_0.10.7_linux_amd64.zip
+  wget https://releases.hashicorp.com/terraform/0.11.7/terraform_0.11.7_linux_amd64.zip
   unzip terraform_0.10.7_linux_amd64.zip
   sudo mv terraform /usr/local/bin/terraform
 )
