1ca994bdb95ca695b0f6fb0f557b12cc94d7b25e
Ian Blenke <ian@blenke.com>
2018-05-14 10:28:07 -0400

    Set some s3 storage archival parameters

--- aws/swx-blueteam/terraform/vpc.tf
+++ aws/swx-blueteam/terraform/vpc.tf
@@ -483,17 +483,17 @@ resource "aws_s3_bucket" "blueteam" {
     enabled = true
 
     noncurrent_version_transition {
-      days          = 30
+      days          = 1825
       storage_class = "STANDARD_IA"
     }
 
     noncurrent_version_transition {
-      days          = 60
+      days          = 3650
       storage_class = "GLACIER"
     }
 
     noncurrent_version_expiration {
-      days = 180
+      days = 7300
     }
   }
 
