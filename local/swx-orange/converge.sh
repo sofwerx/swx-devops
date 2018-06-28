5b93cb8dbac370dce05c0934c7eaf70eb3a243ff
Ian Blenke <ian@blenke.com>
2018-06-28 16:20:14 -0400

    Whitespace!

--- local/swx-orange/converge.sh
+++ local/swx-orange/converge.sh
@@ -3,7 +3,7 @@
 seq 0 7 | while read number ; do
   swx dm env swx-u-ub-orange$number
   docker-compose build setup_kibana
-  docker-compose up -d elasticsearch kibana 
+  docker-compose up -d elasticsearch kibana
   docker-compose up -d --force-recreate setup_kibana
   docker-compose build metricbeat setup_metricbeat logstash setup_logstash
   docker-compose up -d --force-recreate setup_metricbeat metricbeat logstash setup_logstash
