5eb282b3ca2a68975b9194fdefe312a8806c9e5b
Ian Blenke <ian@blenke.com>
2018-06-28 15:39:12 -0400

    Updating comments

--- local/swx-orange/terraform/vpc.tf
+++ local/swx-orange/terraform/vpc.tf
@@ -15,7 +15,7 @@ data "aws_route53_zone" "selected" {
   private_zone = false
 }
 
-/* Define a orange.devwerx.org CNAME record */
+/* Define a orange.opswerx.org CNAME record */
 resource "aws_route53_record" "project-name-cname" {
   zone_id = "${data.aws_route53_zone.selected.zone_id}"
   name    = "${var.Lifecycle}.${var.dns_zone}"
@@ -24,7 +24,7 @@ resource "aws_route53_record" "project-name-cname" {
   records = ["172.109.143.82"]
 }
 
-/* Define a *.orange.devwerx.org CNAME record */
+/* Define a *.orange.opswerx.org CNAME record */
 resource "aws_route53_record" "project-name-wildcard" {
   zone_id = "${data.aws_route53_zone.selected.zone_id}"
   name    = "*.${var.Lifecycle}.${var.dns_zone}"
