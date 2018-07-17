8ae32950fd21b8b2d2b6ece0030658335d8c354d
Ian Blenke <ian@blenke.com>
2018-07-17 15:47:47 -0400

    Cleaning up tor-vpin, moving to submodule

--- aws/tor-vpin/README.md
+++
@@ -1,4 +0,0 @@
-# tor-vpin
-
-The Onion Router (TOR) as a private Virtual Pi Network (VPiN)
-
--- aws/tor-vpin/terraform/tf.sh
+++
@@ -1,3 +0,0 @@
-if [ ! -d .terraform ] ; then
-  terraform init --backend-config="key=tor/vpin/terraform.tfstate"
-fi
