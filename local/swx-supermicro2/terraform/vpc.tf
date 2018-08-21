9324dd328586719cb31e0c06b3f97c1c26942172
Ian Blenke <ian@blenke.com>
2018-08-21 16:11:32 -0400

    Adding terraform for swx-supermicro2

---
+++ local/swx-supermicro2/terraform/vpc.tf
@@ -0,0 +1,35 @@
+/******************************************************************************
+ *
+ * vpc.tf - IAM profile, security groups, and instances
+ *
+ ******************************************************************************/
+
+provider "aws" {
+  region = "${var.aws_region}"
+  access_key = "${var.aws_access_key_id}"
+  secret_key = "${var.aws_secret_access_key}"
+}
+
+data "aws_route53_zone" "selected" {
+  name         = "${var.dns_zone}"
+  private_zone = false
+}
+
+/* Define a supermicro2.opswerx.org CNAME record */
+resource "aws_route53_record" "project-name-cname" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "${var.Lifecycle}.${var.dns_zone}"
+  type    = "A"
+  ttl     = "300"
+  records = ["172.109.143.82"]
+}
+
+/* Define a *.supermicro2.opswerx.org CNAME record */
+resource "aws_route53_record" "project-name-wildcard" {
+  zone_id = "${data.aws_route53_zone.selected.zone_id}"
+  name    = "*.${var.Lifecycle}.${var.dns_zone}"
+  type    = "A"
+  ttl     = "300"
+  records = ["172.109.143.82"]
+}
+
