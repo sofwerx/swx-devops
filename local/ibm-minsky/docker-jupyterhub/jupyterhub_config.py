91957a80a422c47e521b27b82d89142eb58e99e8
Mike Ahern <mike.ahern@sofwerx.org>
2018-09-05 13:17:09 -0400

    changes to allow minio to work with jupyterhub

--- local/ibm-minsky/docker-jupyterhub/jupyterhub_config.py
+++ local/ibm-minsky/docker-jupyterhub/jupyterhub_config.py
@@ -35,7 +35,7 @@ notebook_dir = os.environ.get('DOCKER_NOTEBOOK_DIR') or '/home/jovyan/work'
 c.DockerSpawner.notebook_dir = notebook_dir
 # Mount the real user's Docker volume on the host to the notebook user's
 # notebook directory in the container
-c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir }
+c.DockerSpawner.volumes = { 'jupyterhub-user-{username}': notebook_dir, 'ibmminsky_minio': '/home/jovyan/minio' }
 # volume_driver is no longer a keyword argument to create_container()
 # c.DockerSpawner.extra_create_kwargs.update({ 'volume_driver': 'local' })
 # Remove containers once they are stopped
