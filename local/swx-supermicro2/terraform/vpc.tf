ff262ce9071099aba92d6cd59dbb839e47024f8e
Mike Ahern <mike.ahern@sofwerx.org>
2018-10-05 21:21:20 +0000

    updating supermicro2 to new public ip

--- local/swx-supermicro2/terraform/vpc.tf
+++ local/swx-supermicro2/terraform/vpc.tf
@@ -21,7 +21,7 @@ resource "aws_route53_record" "project-name-cname" {
   name    = "${var.Lifecycle}.${var.dns_zone}"
   type    = "A"
   ttl     = "300"
-  records = ["172.109.143.82"]
+  records = ["172.109.152.118"]
 }
 
 /* Define a *.supermicro2.opswerx.org CNAME record */
@@ -30,6 +30,6 @@ resource "aws_route53_record" "project-name-wildcard" {
   name    = "*.${var.Lifecycle}.${var.dns_zone}"
   type    = "A"
   ttl     = "300"
-  records = ["172.109.143.82"]
+  records = ["172.109.152.118"]
 }
 
